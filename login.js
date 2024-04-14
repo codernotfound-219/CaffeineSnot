39fb0fe4e03073ebdf90fcf268ea762b1f08065c
codernotfound-219 <aswatt219@icloud.com>
2024-04-14 06:59:07 +0530

    landing page

---
+++ login.js
@@ -0,0 +1,8 @@
+
+function loginProvider() {
+    document.querySelector(".popup").classList.add("active");
+}
+
+function closure() {
+    document.querySelector(".popup").classList.remove("active");
+}
